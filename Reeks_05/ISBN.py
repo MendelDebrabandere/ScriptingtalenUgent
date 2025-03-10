df6f44e578cb6082ed6e78ee7bc861d43099d497
MendelDebrabandere <mendel.debrabandere@hotmail.com>
2025-03-10 21:31:21 +0100

    Week5 oef deel 1

---
+++ Reeks_05/ISBN.py
@@ -0,0 +1,77 @@
+﻿from urllib.request import urlopen
+import xml.etree.ElementTree as ET
+
+def isISBN(code):
+    """
+    >>> isISBN('9789027439642')
+    True
+    """
+
+    if len(code) != 13 or code[:3] not in {'978' , '979'}:
+        return False
+
+    o = sum(int(i) for i in code[:-1:2])
+    e = sum(int(i) for i in code[1:-1:2])
+
+    return int(code[-1]) == (10 - (o + 3*e) % 10) % 10
+
+
+
+def display_book_info(ISBNstring):
+    """
+    >>> display_book_info('9780136110675')
+    Title: The Practice of Computing using Python
+    Authors: William F Punch, Richard Enbody
+    Publisher: Addison Wesley
+    >>> display_book_info('9780136110678')
+    Wrong ISBN-13 code
+    """
+
+
+    # check of het wel een isbn13 code is
+    if not isISBN(ISBNstring):
+        print('Wrong ISBN-13 code')
+        return
+
+    # get file from internet
+    url = 'https://pythia.ugent.be/pythia-share/exercises/isbn9/books.php?isbn={}'
+    with urlopen(url.format(ISBNstring)) as file:
+        # parse xml
+        tree = ET.parse(file)
+        root = tree.getroot()
+
+        # get data
+        title = root.find(".//Title").text
+        authors = root.find(".//AuthorsText").text
+        publisher = root.find(".//PublisherText").text
+
+        print("Title:", title)
+        print("Authors:", authors.strip(' ').strip(','))
+        print("Publisher:", publisher)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
+
+
+
+
+
+
+
+
---
+++ Reeks_05/RorschachTest.py
@@ -0,0 +1,39 @@
+﻿
+def rorschach(fileName, output=None):
+    """
+    >>> rorschach('pattern.txt')
+    ++...+..+++..+++..+...++
+    ....++............++....
+    .+.#####+######+#####.+.
+    ...#@#@#.#@##@#.#@#@#...
+    .+.#####.##@@##.#####.+.
+    ....+.+...####...+.+....
+    .....+............+.....
+    ....+.....####.....+....
+    ....+..+..#@@#..+..+....
+    ......###.####.###......
+    +...++#@#.+..+.#@#++...+
+    ....#####......#####....
+    ....#@#....++....#@#....
+    ++..###.+......+.###..++
+    +.....+..........+.....+
+    >>> rorschach('pattern.txt', 'rorschach.txt')
+    """
+
+    outputstr = ''
+    with open(fileName, 'r') as file:
+        for line in file:
+            line = line.rstrip()
+            outputstr += line + line[::-1] + '\n'
+
+
+    if output is not None:
+        with open(output, 'w') as out_file:
+            out_file.write(outputstr)
+    else:
+        print(outputstr.strip())
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
