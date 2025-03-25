ef9e8d02ba71734f61f06e422bb963815a592404
MendelDebrabandere <mendel.debrabandere@hotmail.com>
2025-03-25 14:34:00 +0100

    last prep for eval

--- Python/EvalPrep/EasterEgg.py
+++ Python/EvalPrep/EasterEgg.py
@@ -112,7 +112,7 @@
 
 
 
-    def __init__(self, m, n, start, file_name):R
+    def __init__(self, m, n, start, file_name):
         self.m = m
         self.n = n
         self.bunny_pos = EasterEgg.to_num_coords(start)
